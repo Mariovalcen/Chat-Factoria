19d7c1f5f08abaf70ff7a82fedaff6d4b4d117d7
Mariovalcen <mariovalcen@gmail.com>
2023-02-27 13:20:35 +0100

    Chat template and design

--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -12,34 +12,30 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+
+        @stack ('css')
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <x-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+        <div class="h-32 bg-orange-500">
+        </div>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
+        <div class="absolute left-0 top-6 w-screen">
+            <div class="container mx-auto">
                 {{ $slot }}
-            </main>
+            </div>
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
+        @stack('js')
+
     </body>
 </html>
