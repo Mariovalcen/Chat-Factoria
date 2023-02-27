9e00d31e05724cdf745dcb55ca8af60c45185855
Mariovalcen <mariovalcen@gmail.com>
2023-02-27 18:36:15 +0100

    Change side and color of sent and received messages

--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -130,14 +130,14 @@
                 <div class="h-[calc(100vh-11rem)] px-3 py-2 overflow-auto">
                     {{-- El contenido de nuestro chat --}}
                     @foreach ($this->messages as $message)
-                        <div class="flex justify-end">
+                        <div class="flex {{ $message->user_id == auth()->id() ? 'justify-end' : '' }} mb-2">
 
-                            <div class="rounded px-3 py-2 bg-orange-100">
+                            <div class="rounded px-3 py-2 {{ $message->user_id == auth()->id() ? 'bg-orange-100' : 'bg-gray-200' }} ">
                                 <p class="text-sm">
                                     {{ $message->body }}
                                 </p>
 
-                                <p class="text-right' text-xs text-gray-600 mt-1">
+                                <p class="{{ $message->user_id == auth()->id() ? 'text-right' : '' }} text-xs text-gray-600 mt-1">
                                     {{ $message->created_at->format('d-m-y h:i A') }}
                                 </p>
                             </div>
