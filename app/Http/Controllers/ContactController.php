b5f917b3ee752f8c5f793119eaf7a17985f3bb47
Mariovalcen <mariovalcen@gmail.com>
2023-02-27 11:21:46 +0100

    Adding validation rule for emails already registered. Changing the contact controlles with the new rule

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,8 @@ use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Validation\Rule;
 
+use App\Rules\InvalidEmail;
+
 class ContactController extends Controller
 {
     /**
@@ -47,7 +49,7 @@ class ContactController extends Controller
                 'email',
                 'exists:users',
                 Rule::notIn([auth()->user()->email]),
-            
+                new InvalidEmail
             ]
         ]);
 
@@ -93,6 +95,7 @@ class ContactController extends Controller
                 'email',
                 'exists:users',
                 Rule::notIn([auth()->user()->email]),
+                new InvalidEmail($contact->user->email)
             ]
         ]);
 
